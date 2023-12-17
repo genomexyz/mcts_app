01bfaba7f0abe5af603c1b74f5d1e8150c313c9c
genomexyz <nayrhum@gmail.com>
2023-12-17 23:39:37 +0800

    add gui

---
+++ gui/run.py
@@ -0,0 +1,46 @@
+# Importing the Flask module
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+from io import BytesIO
+import pandas as pd
+from flask import Flask, send_file
+
+import chess
+import chess.pgn
+
+
+
+# Creating a Flask web application
+app = Flask(__name__)
+
+# Defining a route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get_move', methods=['POST'])
+def get_move():
+    #begin chess
+    board = chess.Board()
+    # Get the JSON data from the request
+    json_data = request.get_json()
+    hist_move = json_data['history']
+
+    res = {}
+    res['legal'] = False
+    for iter_hist in range(len(hist_move)):
+        single_hist = hist_move[iter_hist]
+        legal_move = list(board.legal_moves)
+        legal_move_str = []
+        for iter_legal in range(len(legal_move)):
+            legal_move_str.append(str(legal_move[iter_legal]))
+        
+        if single_hist not in legal_move_str:
+            return res
+    
+    res['legal'] = True
+    return res
+
+# Running the application on the local development server
+if __name__ == '__main__':
+    app.run(debug=True)
